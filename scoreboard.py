eb9f4d3c0992a696498095e3120e4f585c3a1a3d
Karim Bouaziz <karimbouaziz@Karims-Laptop.fritz.box>
2024-07-18 15:56:06 +0200

    Store the highest achieved score in a txt file, and read it every new start of a game

--- scoreboard.py
+++ scoreboard.py
@@ -1,12 +1,17 @@
 from turtle import Turtle
 from configuration import config
 
+HIGH_SCORE_FILE = "highscore.txt"
 
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        try:
+            with open(HIGH_SCORE_FILE) as score_file:            
+                self.high_score = int(score_file.read())
+        except Exception:
+            self.high_score = 0
         self.penup()
         self.hideturtle()
         self.color("white")
@@ -39,3 +44,5 @@ class ScoreBoard(Turtle):
         self.write(f"Game over. Restart loading...", move=False, align="center", font=('Arial', 20, 'normal'))
         self.goto(0, -20)
         self.write(f"Final score: {self.score}", move=False, align="center", font=('Arial', 20, 'normal'))
+        with open(HIGH_SCORE_FILE, mode="w") as score_file:
+            score_file.write(str(self.high_score))
